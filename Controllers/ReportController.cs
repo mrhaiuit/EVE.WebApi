7ef1a45908b6edfab6c204090a432a36dd177458
david <davidtrinh173@gmail.com>
2020-03-18 07:22:22 +0700

    commit;

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -4,10 +4,11 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using AutoMapper;
-
+using EVE.ApiModels.Authentication.Request;
 using EVE.Bussiness;
 using EVE.Commons;
 using EVE.Data;
+using EVE.WebApi.Authentication.Helper;
 using EVE.WebApi.Shared;
 using EVE.WebApi.Shared.Response;
 
@@ -21,31 +22,6 @@ namespace EVE.WebApi.Controllers
                                IMapper mapper) : base(mapper)
         {
             _loginBE = loginBE;
-        }
-
-        [HttpPost]
-        [Route("logon")]
-        public async Task<HttpResponseMessage> Logon(LoginReq req)
-        {
-            var employee = await _loginBE.GetEmployeeByAccount(req);
-            if (employee != null)
-            {
-                var curTime = DateTime.Now;
-                var logonUser = new LoginUser
-                {
-                    UserName = employee.UserName,
-                    UserId = employee.EmployeeCode,
-                    IpAddress = AppUtil.GetClientIp(Request),
-                    LoginDate = curTime,
-                    UpdTs = curTime
-                };
-
-               await _loginBE.SaveLogin(logonUser);
-
-                return this.OkResult(logonUser);
-            }
-
-            return this.ErrorResult(new Error(EnumError.LogonInvalid));
         }
 
 
